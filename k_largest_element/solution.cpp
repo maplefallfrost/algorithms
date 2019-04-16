e62666c8be2cb18d8bcc5939815366ac268e4804
Junyu Ye <maplefallfrost@gmail.com>
2019-04-16 15:21:48 +0800

    lintcode 5 k largest element

---
+++ k_largest_element/solution.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    /**
+     * @param n: An integer
+     * @param nums: An array
+     * @return: the Kth largest element
+     */
+    int quickSelect(vector<int> &nums, int start, int end, int n) {
+        if (start == end) {
+            return nums[start];
+        }
+        int pivot = nums[(start + end) / 2];
+        int i = start, j = end;
+        while (i <= j) {
+            while (i <= j && nums[i] > pivot) {
+                i++;
+            }
+            while (i <= j && nums[j] < pivot) {
+                j--;
+            }
+            if (i <= j) {
+                swap(nums[i], nums[j]);
+                i++;
+                j--;
+            }
+        }
+        
+        if (j - start + 1 >= n) {
+            return quickSelect(nums, start, j, n);
+        }
+        if (i - start + 1 <= n) {
+            return quickSelect(nums, i, end, n - (i - start));
+        }
+        return nums[j + 1];
+    }
+    int kthLargestElement(int n, vector<int> &nums) {
+        // write your code here
+        if (nums.size() == 0) {
+            return -1;
+        }
+        return quickSelect(nums, 0, nums.size() - 1, n);
+    }
+};
